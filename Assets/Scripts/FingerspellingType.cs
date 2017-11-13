eec9378800faf9a9c80f5954767883205ad8d0d7
j13417 <j13417@j13417-no-MacBook-Air.local>
2017-11-13 11:02:49 +0900

    frist animation system

---
+++ Assets/Scripts/FingerspellingType.cs
@@ -0,0 +1,60 @@
+﻿//using System.Collections;
+
+public enum FingerspellingType {
+	NONE = -1,
+	WORD = -2,
+	A = 0,
+	I,
+	U,
+	E,
+	O, //4
+	KA,
+	KI,
+	KU,
+	KE,
+	KO,
+
+	SA,
+	SI,
+	SU,
+	SE,
+	SO,
+
+	TA,
+	TI,
+	TU,
+	TE,
+	TO,
+
+	NA,
+	NI,
+	NU,
+	NE,
+	NO,
+
+	HA,
+	HI,
+	HU,
+	HE,
+	HO,
+
+	MA,
+	MI,
+	MU,
+	ME,
+	MO,
+
+	YA,
+	YU,
+	YO,
+
+	RA,
+	RI,
+	RU,
+	RE,
+	RO,
+
+	WA,
+	WO,
+	NN,
+}
---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+	[SerializeField]
+	FingerspellingType type;
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponentsInChildren<Animator>(true)[0];
+		animator.Play (type.ToString ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
